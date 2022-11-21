f56cfe21b55e5b3bf0aeceece262f2fcdcfde3c8
Miguel A. Garcia <miguelangelgarcia994@gmail.com>
2022-11-21 19:20:00 +0100

    api and team list apiservice

--- routes/teams.routes.js
+++ routes/teams.routes.js
@@ -2,21 +2,24 @@ const express = require("express")
 const router = express.Router()
 
 
-// const teamsApi = require("./../services/teams-api.service")
-// const api = new teamsApi()
+const teamsApi = require("./../services/teams-api.service")
+const api = new teamsApi()
 
 router.get("/equipos", (req, res, next) => {
-    res.render("teams/team-list")
-  //  api
-  //     .getAllTeams()
-  //     .then(response => res.render('teams/teams-list', { teams: response.data }))
-  //     .catch(err => console.log(err))
+  /*  res.render("teams/team-list") */
+  api
+    .getAllTeams()
+    .then(response => {
+      console.log('-----', response.data)
+      res.render('teams/teams-list', response.data)
+    })
+    .catch(err => console.log(err))
 })
 
 
 
-router.get("/equipos/detalles", (req, res, next) => {
-    res.render("teams/team-details")
-})
+/* router.get("/equipos/detalles", (req, res, next) => {
+  res.render("teams/team-details")
+}) */
 
 module.exports = router
